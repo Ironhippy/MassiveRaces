f08759d5ab4b65c44706d03f45cdfc60e8d55506
ironh <ironh@DESKTOP-CU8PMIS.fios-router.home>
2017-07-25 18:42:06 -0400

    Added trait editing functionality in-game

---
+++ Races/src/net/graystone/java/races/command/edit/trait/CmdTrait.java
@@ -0,0 +1,25 @@
+package net.graystone.java.races.command.edit.trait;
+
+import java.util.List;
+
+import com.massivecraft.massivecore.util.MUtil;
+
+import net.graystone.java.races.command.RaceCommand;
+
+public class CmdTrait extends RaceCommand
+{
+	
+	public CmdTrait()
+	{
+		this.setDesc("edit a race's trait settings");
+		
+		this.addChild(new CmdTraitAdd());
+	}
+	
+	@Override
+	public List<String> getAliases()
+	{
+		return MUtil.list("trait");
+	}
+	
+}
