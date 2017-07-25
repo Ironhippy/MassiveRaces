605b9b95669c62e84175bdb0e9103533129d1ac1
ironh <ironh@DESKTOP-CU8PMIS.fios-router.home>
2017-07-25 18:44:19 -0400

    Added trait removal functionality in-game

---
+++ Races/src/net/graystone/java/races/command/edit/trait/CmdTraitRemove.java
@@ -0,0 +1,36 @@
+package net.graystone.java.races.command.edit.trait;
+
+import com.massivecraft.massivecore.MassiveException;
+import com.massivecraft.massivecore.util.Txt;
+
+import net.graystone.java.races.RaceTrait;
+import net.graystone.java.races.command.RaceCommand;
+import net.graystone.java.races.command.type.TypeRace;
+import net.graystone.java.races.command.type.TypeTrait;
+import net.graystone.java.races.entity.MRace;
+
+public class CmdTraitRemove extends RaceCommand
+{
+	public CmdTraitRemove()
+	{
+		this.addAliases("remove");
+		this.setDesc("remove a race trait");
+		
+		this.addParameter(TypeRace.get(), "raceName");
+		this.addParameter(TypeTrait.get(), "traitId");
+	}
+	
+	@Override
+	public void perform() 
+			throws MassiveException
+	{
+		MRace targetRace = this.readArg();
+		RaceTrait trait = this.readArg();
+		
+		if (targetRace.containsTrait(trait)) { message(Txt.parse("<rose>The <pink> "+targetRace.getName()+" race <rose>doesn not have the <pink>"+trait.name()+" trait<rose>.")); return; }
+		
+		targetRace.removeTrait(trait);
+		
+		message(Txt.parse("<i>Successfully removed the <pink>"+trait.toString()+" trait <i>from the <pink>"+targetRace.getName()+" race<i>."));
+	}
+}
