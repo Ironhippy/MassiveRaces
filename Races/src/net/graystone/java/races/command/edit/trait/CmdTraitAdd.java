5ae9f715175e4a41e6d4e1966c4f191ef7eb54e7
ironh <ironh@DESKTOP-CU8PMIS.fios-router.home>
2017-07-25 18:42:18 -0400

    Added trait appending abilities in-game

---
+++ Races/src/net/graystone/java/races/command/edit/trait/CmdTraitAdd.java
@@ -0,0 +1,37 @@
+package net.graystone.java.races.command.edit.trait;
+
+import com.massivecraft.massivecore.MassiveException;
+import com.massivecraft.massivecore.util.Txt;
+
+import net.graystone.java.races.RaceTrait;
+import net.graystone.java.races.command.RaceCommand;
+import net.graystone.java.races.command.type.TypeRace;
+import net.graystone.java.races.command.type.TypeTrait;
+import net.graystone.java.races.entity.MRace;
+
+public class CmdTraitAdd extends RaceCommand
+{
+	
+	public CmdTraitAdd()
+	{
+		this.addAliases("add");
+		this.setDesc("add a race trait");
+		
+		this.addParameter(TypeRace.get(), "raceName");
+		this.addParameter(TypeTrait.get(), "traitId");
+	}
+	
+	@Override
+	public void perform() 
+			throws MassiveException
+	{
+		MRace targetRace = this.readArg();
+		RaceTrait trait = this.readArg();
+		
+		if (targetRace.containsTrait(trait)) { message(Txt.parse("<rose>The <pink> "+targetRace.getName()+" race <rose>already has the <pink>"+trait.name()+" trait<rose>.")); return; }
+		
+		targetRace.addTrait(trait);
+		
+		message(Txt.parse("<i>Successfully added the <pink>"+trait.toString()+" trait <i>to the <pink>"+targetRace.getName()+" race<i>."));
+	}
+}
