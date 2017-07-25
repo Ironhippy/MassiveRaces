3e65bfe3ed72824ae4cf05a9c344fa9d9d6f2ac6
ironh <ironh@DESKTOP-CU8PMIS.fios-router.home>
2017-07-25 14:00:23 -0400

    Fixed username list mixup with MStore databases

--- Races/src/net/graystone/java/races/entity/MPlayerColl.java
+++ Races/src/net/graystone/java/races/entity/MPlayerColl.java
@@ -1,5 +1,9 @@
 package net.graystone.java.races.entity;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import com.massivecraft.massivecore.store.MStore;
 import com.massivecraft.massivecore.store.SenderColl;
 
@@ -15,4 +19,17 @@ public class MPlayerColl extends SenderColl<MPlayer>
 	{
 		super("massiveraces_mplayers", MPlayer.class, MStore.getDb(), MassiveRaces.get());
 	}
+	
+	public Collection<String> getPlayerNames()
+	{
+		List<String> collNames = new ArrayList<String>();
+		
+		for (MPlayer players : get().getAll())
+		{
+			collNames.add(players.getName());
+		}
+		
+		return collNames;
+	}
+	
 }
