5671a7d5840fc2cb72fb53c976f0761883f9f52d
ironh <ironh@DESKTOP-CU8PMIS.fios-router.home>
2017-07-26 11:01:31 -0400

    Added NullPointerException catch

--- Races/src/net/graystone/java/races/engine/LoginEngine.java
+++ Races/src/net/graystone/java/races/engine/LoginEngine.java
@@ -1,13 +1,11 @@
 package net.graystone.java.races.engine;
 
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import com.massivecraft.massivecore.Engine;
 import com.massivecraft.massivecore.util.Txt;
 
-import net.graystone.java.races.MassiveRaces;
 import net.graystone.java.races.entity.MConf;
 import net.graystone.java.races.entity.MPlayer;
 import net.graystone.java.races.entity.MRace;
@@ -19,24 +17,12 @@ public class LoginEngine extends Engine
 	private static LoginEngine i = new LoginEngine();
 	public static LoginEngine get() { return LoginEngine.i; }
 	
-	@EventHandler(priority=EventPriority.HIGH)
-	public void setLogin(PlayerJoinEvent event)
-	{
-		MPlayer player = MPlayer.get(event.getPlayer());
-		
-		if (player.getRace()!=null) return;
-		
-		player.setRace(MRaceColl.get().get(MConf.get().getDefaultRace()));
-		
-		MassiveRaces.get().log("<i>Successfully registered <pink>"+player.getName()+" <i>with the <pink>default race<i>.");
-	}
-	
 	@EventHandler
 	public void messageLogin(PlayerJoinEvent event)
 	{
 		MPlayer player = MPlayer.get(event.getPlayer());
 		
-		if (player.getRace()==null) return;
+		if (player.getRace()==null) { player.setRace(MRaceColl.get().get(MConf.get().getDefaultRace())); }
 		
 		MRace race = player.getRace();
 		
