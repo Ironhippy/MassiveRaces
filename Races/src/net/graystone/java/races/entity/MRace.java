ed06b984a7ed3e0bb016b63434bebcee41909929
ironh <ironh@DESKTOP-CU8PMIS.fios-router.home>
2017-07-26 10:25:17 -0400

    Added default boolean

--- Races/src/net/graystone/java/races/entity/MRace.java
+++ Races/src/net/graystone/java/races/entity/MRace.java
@@ -8,12 +8,18 @@ import org.bukkit.potion.PotionEffectType;
 import com.massivecraft.massivecore.collections.MassiveList;
 import com.massivecraft.massivecore.store.Entity;
 
+import net.graystone.java.races.RaceTrait;
+
 public class MRace extends Entity<MRace>
 {
 	
 	private String displayName;
 	public MRace setName(String arg0) { this.displayName = arg0; this.changed(); return this; }
 	public String getName() { return this.displayName; }
+	
+	protected boolean isDefault = false;
+	public boolean isDefault() { return this.isDefault; }
+	public void setDefault(boolean set) { this.isDefault = set; this.changed(); }
 	
 	private List<String> subraceIds = new MassiveList<String>();
 	public boolean addSubrace(MRace arg0) { if (containsSubrace(arg0)) return false; this.subraceIds.add(arg0.getId()); this.changed(); return true; }
@@ -50,6 +56,12 @@ public class MRace extends Entity<MRace>
 	public boolean hasParticleEffect() { if(particleEffect == null) return false; return true; }
 	
 	public boolean equals(MRace race) { if (this.getId().equalsIgnoreCase(race.getId())) return true; return false; }
+	
+	private List<String> raceTraits = new MassiveList<String>();
+	public void addTrait(RaceTrait arg0) { this.raceTraits.add(arg0.toString()); this.changed(); }
+	public void removeTrait(RaceTrait arg0) { this.raceTraits.add(arg0.toString()); this.changed(); }
+	
+	public boolean containsTrait(RaceTrait arg0) { if (raceTraits.contains(arg0.toString())) return true; return false; }
 	
 	private String raceDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac molestie magna, semper elementum est. Mauris quis hendrerit odio. Curabitur sollicitudin, velit sit amet bibendum iaculis, sem quam ullamcorper mauris, ac rhoncus mauris augue non tortor. Quisque in enim vulputate nibh scelerisque mollis. Phasellus quis elementum quam. Sed a malesuada velit. Curabitur vel rutrum lectus. Duis porttitor elit et purus volutpat, sit amet aliquam neque volutpat. Suspendisse ligula mauris, finibus sit amet metus sagittis, ornare blandit purus. Aliquam a felis ac lacus porttitor dapibus sed id ligula. In at molestie ipsum. Aenean mollis eros dui, feugiat convallis felis egestas at.";
 	public MRace setDesc(String arg0) { this.raceDescription = arg0; this.changed(); return this; }
