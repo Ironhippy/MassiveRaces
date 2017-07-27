9e4c7e9440849a6cd6a1756116ad60ad31608756
ironh <ironh@DESKTOP-CU8PMIS.fios-router.home>
2017-07-27 10:15:12 -0400

    Rewriting trait handle

--- Races/src/net/graystone/java/races/engine/TraitEngine.java
+++ Races/src/net/graystone/java/races/engine/TraitEngine.java
@@ -1,8 +1,6 @@
 package net.graystone.java.races.engine;
 
-import java.util.Map;
-
-import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -10,19 +8,15 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import com.massivecraft.massivecore.Engine;
-import com.massivecraft.massivecore.collections.MassiveMap;
 
-import net.graystone.java.races.MassiveRaces;
+
 import net.graystone.java.races.RaceTrait;
-import net.graystone.java.races.entity.MConf;
 import net.graystone.java.races.entity.MPlayer;
 import net.graystone.java.races.entity.MRace;
 import net.graystone.java.races.event.LightChangeEvent;
 
 public class TraitEngine extends Engine
 {
-	
-	private Map<String, Integer> scheduledTasks = new MassiveMap<String, Integer>();
 	
 	private static TraitEngine i = new TraitEngine();
 	public static TraitEngine get() { return TraitEngine.i; }
@@ -42,114 +36,23 @@ public class TraitEngine extends Engine
 	{
 		MPlayer target = event.getPlayer();
 		MRace playerRace = target.getRace();
-		if (!playerRace.containsTrait(RaceTrait.FREEZE_DARKNESS) && !playerRace.containsTrait(RaceTrait.BURN_SUNLIGHT) &&
-			!playerRace.containsTrait(RaceTrait.FEED_DARKNESS) && !playerRace.containsTrait(RaceTrait.FEED_SUNLIGHT) &&
-			!playerRace.containsTrait(RaceTrait.HEAL_DARKNESS) && !playerRace.containsTrait(RaceTrait.HEAL_SUNLIGHT)) return;
+		
+		if (!playerRace.containsTrait(RaceTrait.BURN_SUNLIGHT) && !playerRace.containsTrait(RaceTrait.FEED_SUNLIGHT)) return;
 		
 		Player player = event.getPlayer().getPlayer();
+		if (!hasSun(player)) return;
 		
-		// Feed darkness
-		if (playerRace.containsTrait(RaceTrait.FEED_DARKNESS) && !isLight(event.getLight()))
+		if (playerRace.containsTrait(RaceTrait.BURN_SUNLIGHT))
 		{
-			long hungerPeriodRemaining = (20-player.getFoodLevel())*20L;
 			
-			if (hungerPeriodRemaining == 20) return;
-			
-			Bukkit.getScheduler().scheduleSyncRepeatingTask(MassiveRaces.get(), new Runnable()
-			{
-			    public void run()
-			    {
-			    	player.setFoodLevel(player.getFoodLevel()+1);
-			    }
-			}, 20L, hungerPeriodRemaining);
-		}
-		
-		// Feed sunlight
-		else if (playerRace.containsTrait(RaceTrait.FEED_SUNLIGHT) && isLight(event.getLight()))
-		{
-			long hungerPeriodRemaining = (20-player.getFoodLevel())*20L;
-			
-			if (hungerPeriodRemaining == 20) return;
-			
-			Bukkit.getScheduler().scheduleSyncRepeatingTask(MassiveRaces.get(), new Runnable()
-			{
-			    public void run()
-			    {
-			    	player.setFoodLevel(player.getFoodLevel()+1);
-			    }
-			}, 20L, hungerPeriodRemaining);
-		}
-		
-		// Heal in darkness
-		if (playerRace.containsTrait(RaceTrait.HEAL_DARKNESS) && !isLight(event.getLight()))
-		{
-			long healthPeriodRemaining = (long) ((player.getHealthScale()-player.getHealth())*20L);
-			
-			Bukkit.getScheduler().scheduleSyncRepeatingTask(MassiveRaces.get(), new Runnable()
-			{
-			    public void run()
-			    {
-			    	player.setHealth(player.getHealth()+1);
-			    }
-			}, 20L, healthPeriodRemaining);
-		}
-		
-		// Heal in sunlight
-		else if (playerRace.containsTrait(RaceTrait.HEAL_SUNLIGHT) && isLight(event.getLight()))
-		{
-			long healthPeriodRemaining = (long) ((player.getHealthScale()-player.getHealth())*20L);
-			
-			Bukkit.getScheduler().scheduleSyncRepeatingTask(MassiveRaces.get(), new Runnable()
-			{
-			    public void run()
-			    {
-			    	player.setHealth(player.getHealth()+1);
-			    }
-			}, 20L, healthPeriodRemaining);
-		}
-		
-		// Freeze in darkness
-		if (playerRace.containsTrait(RaceTrait.FREEZE_DARKNESS) && !isLight(event.getLight()))
-		{
-			Bukkit.getScheduler().scheduleSyncRepeatingTask(MassiveRaces.get(), new Runnable()
-			{
-				public void run()
-				{
-					if (isLight(event.getLight()))
-					{
-						int cancelled = scheduledTasks.get(event.getPlayer().getId());
-						Bukkit.getScheduler().cancelTask(cancelled);
-						return;
-					}
-					player.damage(1D);
-				}
-			}, 20L, 0L);
-		}
-		
-		// Burn in sunlight
-		else if (playerRace.containsTrait(RaceTrait.BURN_SUNLIGHT) && isLight(event.getLight()))
-		{
-			Bukkit.getScheduler().scheduleSyncRepeatingTask(MassiveRaces.get(), new Runnable()
-			{
-				public void run()
-				{
-					if (!isLight(event.getLight()))
-					{
-						int cancelled = scheduledTasks.get(event.getPlayer().getId());
-						Bukkit.getScheduler().cancelTask(cancelled);
-						return;
-					}
-					player.setFireTicks(20);
-				}
-			}, 20L, 0L);
 		}
 	}
 	
 	@EventHandler(priority=EventPriority.HIGH)
 	public void lightUpdateEvent(PlayerMoveEvent event)
 	{
-		float lightFrom = event.getFrom().getBlock().getLightFromSky();
-		float lightTo = event.getTo().getBlock().getLightFromSky();
+		float lightFrom = event.getFrom().getBlock().getLightLevel();
+		float lightTo = event.getTo().getBlock().getLightLevel();
 		
 		if (lightTo == lightFrom) return;
 		
@@ -157,11 +60,13 @@ public class TraitEngine extends Engine
 		calledEvent.run();
 	}
 	
-	private boolean isLight(float light)
+	private boolean hasSun(Player target)
 	{
-		if (light >= MConf.get().burnAt()) return true;
+		float lightLevel = target.getLocation().getBlock().getLightFromSky();
+		World occupantWorld = target.getLocation().getWorld();
 		
-		return false;
+		if (occupantWorld.getTime()>12500 || lightLevel<11) return false;
+		
+		return true;
 	}
-	
 }
