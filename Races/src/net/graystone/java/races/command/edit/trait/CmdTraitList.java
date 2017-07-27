e5e3e7ddc17ac6ac9e02142a69bbf310efbee99f
ironh <ironh@DESKTOP-CU8PMIS.fios-router.home>
2017-07-27 10:13:02 -0400

    List traits applicable to a race

---
+++ Races/src/net/graystone/java/races/command/edit/trait/CmdTraitList.java
@@ -0,0 +1,36 @@
+package net.graystone.java.races.command.edit.trait;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.massivecraft.massivecore.MassiveException;
+import com.massivecraft.massivecore.util.Txt;
+
+import net.graystone.java.races.command.RaceCommand;
+import net.graystone.java.races.command.type.TypeRace;
+import net.graystone.java.races.entity.MRace;
+
+public class CmdTraitList extends RaceCommand
+{
+	
+	public CmdTraitList()
+	{
+		this.addAliases("list");
+		this.setDesc("list traits applicable to a race");
+		
+		this.addParameter(TypeRace.get(), "raceName");
+	}
+	
+	@Override
+	public void perform() throws MassiveException
+	{
+		MRace targetRace = this.readArg();
+		
+		List<String> traits = targetRace.getTraits();
+		
+		String sepVar = StringUtils.join(traits, "<i>, <aqua>");
+		
+		message(Txt.parse("<i>The <pink>"+targetRace.getName()+" race has the following traits: <aqua>"+sepVar+"<i>."));
+	}
+}
