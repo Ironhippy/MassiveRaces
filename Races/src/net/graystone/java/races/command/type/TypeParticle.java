67942a43b6ebe4b6b801ea986f3fe5adf977d89d
ironh <ironh@DESKTOP-CU8PMIS.fios-router.home>
2017-07-28 21:33:44 -0400

    Made TypeParticle more accurate and serializable

--- Races/src/net/graystone/java/races/command/type/TypeParticle.java
+++ Races/src/net/graystone/java/races/command/type/TypeParticle.java
@@ -1,10 +1,16 @@
 package net.graystone.java.races.command.type;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.bukkit.Particle;
+import org.bukkit.command.CommandSender;
 
-import com.massivecraft.massivecore.command.type.enumeration.TypeEnum;
+import com.massivecraft.massivecore.MassiveException;
+import com.massivecraft.massivecore.collections.MassiveList;
+import com.massivecraft.massivecore.command.type.TypeAbstract;
 
-public class TypeParticle extends TypeEnum<Particle>
+public class TypeParticle extends TypeAbstract<Particle>
 {
 	
 	private static TypeParticle i = new TypeParticle();
@@ -15,4 +21,23 @@ public class TypeParticle extends TypeEnum<Particle>
 		super(Particle.class);
 	}
 	
+	@Override
+	public Particle read(String arg, CommandSender sender)
+			throws MassiveException
+	{	
+		return Particle.valueOf(arg);
+	}
+	
+	@Override
+	public Collection<String> getTabList(CommandSender sender, String arg)
+	{
+		List<String> retTabList = new MassiveList<String>();
+		
+		for (Particle all : Particle.values())
+		{
+			retTabList.add(all.toString());
+		}
+			
+		return retTabList;
+	}
 }
