0793f25304388f6f16011aa88fb7da8dd69d6f80
BuildTools <unconfigured@null.spigotmc.org>
2017-07-30 23:52:05 -0400

    Added 'requirements' boolean, implemented Trait interface

--- Races/src/net/graystone/java/races/traits/TraitAbstract.java
+++ Races/src/net/graystone/java/races/traits/TraitAbstract.java
@@ -2,15 +2,24 @@ package net.graystone.java.races.traits;
 
 import com.massivecraft.massivecore.Engine;
 
-public abstract class TraitAbstract extends Engine
+import net.graystone.java.races.entity.MRace;
+
+public abstract class TraitAbstract extends Engine implements Trait
 {
 	
 	private String id;
-	public String getId() { return this.id; } 
+	public String getId() { return this.id; }
 	
 	public TraitAbstract(String id)
 	{
 		this.id = id;
+	}
+	
+	protected boolean containsTrait(MRace race)
+	{
+		if (!race.containsTrait(this)) return false;
+		
+		return true;
 	}
 	
 }
