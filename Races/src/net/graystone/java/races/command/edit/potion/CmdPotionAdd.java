04f6eab335a1d80f3c4a553947372203d706065a
ironh <ironh@DESKTOP-9CRBS3L.fios-router.home>
2017-08-01 21:46:18 -0400

    Added the ability to edit a races potion effect

--- Races/src/net/graystone/java/races/command/edit/potion/CmdPotionAdd.java
+++ Races/src/net/graystone/java/races/command/edit/potion/CmdPotionAdd.java
@@ -8,9 +8,12 @@ import com.massivecraft.massivecore.MassiveException;
 import com.massivecraft.massivecore.command.requirement.RequirementHasPerm;
 import com.massivecraft.massivecore.command.type.TypePotionEffectType;
 import com.massivecraft.massivecore.util.MUtil;
+import com.massivecraft.massivecore.util.Txt;
 
 import net.graystone.java.races.Perm;
 import net.graystone.java.races.command.RaceCommand;
+import net.graystone.java.races.command.type.TypeRace;
+import net.graystone.java.races.entity.MRace;
 
 public class CmdPotionAdd extends RaceCommand
 {
@@ -22,12 +25,19 @@ public class CmdPotionAdd extends RaceCommand
 		this.addRequirements(RequirementHasPerm.get(Perm.PEFFECT_ADD.toString()));
 		
 		this.addParameter(TypePotionEffectType.get(), "potionEffect");
+		this.addParameter(TypeRace.get(), "targetRace");
 	}
 	
 	@Override
 	public void perform() throws MassiveException
 	{
-		PotionEffectType potiontype = this.readArg();
+		PotionEffectType potionType = this.readArg();
+		MRace race = this.readArg();
+		
+		race.addPotionEffect(potionType);
+		
+		message(Txt.parse("<i>You have successfully added the <pink>"+potionType.getName()
+		       +" <i>to the <pink>"+race.getName()+" race<i>."));
 	}
 	
 	@Override
